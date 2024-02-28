25fdf9449037e8af088886842e90e8097c3629eb
Iman <111653356+Arussa@users.noreply.github.com>
2024-02-28 21:26:08 +0300

    Update main.dart

--- taskfive/lib/main.dart
+++ taskfive/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'animate.dart'; // Import the animate.dart file
 
 void main() {
   runApp(const MyApp());
@@ -6,7 +7,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // ignore: use_key_in_widget_constructors
-  const MyApp({Key? key});
+  const MyApp();
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +36,7 @@ class HomePage extends StatelessWidget {
     {
       'title': 'Yetewega Lib',
       'imageUrl':
-          'https://th.bing.com/th/id/R.eb86e9d73d77618b47390721572c117a?rik=l5wnzSvX%2fXaPoA&riu=http%3a%2f%2fwww.mereb.shop%2fImage%2fProduct%2f49435409%2f49435409-84-903-large.jpg&ehk=T%2ftqAueIhArjco4qeZMRFkBLZVNPzrQsB2LpMKJoJ2I%3d&risl=&pid=ImgRaw&r=0',
+          'https://th.bing.com/th/id/R.eb86e9d73d77618b47390721572c117a?rik=l5wnzSvX%2fXaPoA&riu=http%3a%2f%2fwww.mereb.shop%2fImage%2fProduct%2f49435409%2f49435409-84-903-large.jpg&ehk=T%2ftqAueIhArjco4qeZMRFkBLZVNPzrQsB2LpMKJoJ2I%3d&risl=&pid=ImgRaw',
     },
     {
       'title': 'Lela Sew',
@@ -44,7 +45,8 @@ class HomePage extends StatelessWidget {
     },
   ];
 
-  HomePage({super.key});
+  // ignore: use_key_in_widget_constructors
+  HomePage();
 
   @override
   Widget build(BuildContext context) {
@@ -82,81 +84,6 @@ class HomePage extends StatelessWidget {
               onChanged: (value) {},
             ),
             const SizedBox(height: 16.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.filter),
-                  onPressed: () {
-                    // Filter functionality
-                  },
-                ),
-                const SizedBox(
-                  width: 430.0,
-                  height: 200.0,
-                  child: Card(
-                    color: Colors.blue,
-                    elevation: 1.0,
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(265.0, 1.0, 3.0, 0.0),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Sep 23, 2023',
-                                style: TextStyle(
-                                  fontSize: 12.0,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(70.0, 3.0, 50, 0.0),
-                          child: Row(
-                            children: [
-                              Icon(Icons.pause,
-                                  color: Colors.white, size: 25.0),
-                              SizedBox(width: 27.0),
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(5, 15, 5, 5),
-                                child: Text(
-                                  'Today a reader \n tomorrow a',
-                                  style: TextStyle(
-                                    fontSize: 19,
-                                    color: Color.fromRGBO(225, 225, 221, 1),
-                                    fontWeight: FontWeight.w500,
-                                    letterSpacing: 0.09,
-                                  ),
-                                ),
-                              ),
-                              SizedBox(width: 40.0),
-                              Icon(Icons.pause, color: Colors.white, size: 38.0)
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(30.0, 0.0, 40.0, 0.0),
-                          child: Text(
-                            'LEADER',
-                            style: TextStyle(
-                              fontSize: 45.0,
-                              color: Color.fromRGBO(248, 248, 247, 1),
-                              fontWeight: FontWeight.w400,
-                              letterSpacing: 2.0,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16.0),
             const Text('Categories'),
             const SizedBox(height: 8.0),
             const Wrap(
@@ -176,46 +103,14 @@ class HomePage extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               child: Row(
                 children: [
-                  for (var book in books)
+                  BookIconAnimation(), // Use BookIconAnimation widget for animation
+                  for (var i = 0; i < books.length; i++)
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                      child: BookListItem(
-                        title: book['title']!,
-                        imageUrl: book['imageUrl']!,
-                      ),
-                    ),
-                ],
-              ),
-            ),
-            const Text('New'),
-            const SizedBox(height: 8.0),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  for (var book in books)
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                      child: BookListItem(
-                        title: book['title']!,
-                        imageUrl: book['imageUrl']!,
-                      ),
-                    ),
-                ],
-              ),
-            ),
-            const Text('Trending'),
-            const SizedBox(height: 8.0),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  for (var book in books)
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                      child: BookListItem(
-                        title: book['title']!,
-                        imageUrl: book['imageUrl']!,
+                      child: BookItem(
+                        title: books[i]['title']!,
+                        imageUrl: books[i]['imageUrl']!,
+                        delay: Duration(milliseconds: 200 * i),
                       ),
                     ),
                 ],
@@ -228,37 +123,79 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class BookListItem extends StatelessWidget {
+class BookItem extends StatefulWidget {
   final String title;
   final String imageUrl;
+  final Duration delay;
 
-  const BookListItem({
+  const BookItem({
     Key? key,
     required this.title,
     required this.imageUrl,
+    required this.delay,
   }) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
+  _BookItemState createState() => _BookItemState();
+}
+
+class _BookItemState extends State<BookItem>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeIn,
+      ),
+    );
+    Future.delayed(widget.delay, () {
+      _controller.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.network(
-            imageUrl,
-            height: 120,
-            width: 150,
-            fit: BoxFit.cover,
+    return FadeTransition(
+      opacity: _animation,
+      child: SizedBox(
+        width: 150,
+        child: Card(
+          elevation: 4,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(
+                widget.imageUrl,
+                height: 120,
+                width: 150,
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  widget.title,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              title,
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
